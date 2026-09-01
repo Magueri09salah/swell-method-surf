5d80f42a7e309862247b2483f908b143c2c1881d
Magueri09salah <maguerisalaheddine@gmail.com>
2026-09-01 13:19:42 +0100

    removes google reviews

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -20,7 +20,7 @@ import { getContent } from "@/server/services/content";
 import { getFeaturedOffers } from "@/server/services/coaching";
 import { getActivePackages } from "@/server/services/package";
 import { getFeaturedTestimonials } from "@/server/services/testimonial";
-import { getGoogleReviews } from "@/server/services/google-reviews";
+// import { getGoogleReviews } from "@/server/services/google-reviews";
 import { getGalleryPreview } from "@/server/services/gallery";
 
 export const metadata: Metadata = pageMetadata({
